aabfef1bcc50d47cdf5a83b9db8742b20db93c2d
parvvaresh <parvvaresh@gmail.com>
2025-03-04 07:43:21 +0330

    fix setup

--- setup.py
+++ setup.py
@@ -3,44 +3,22 @@ import os
 
 
 
+from setuptools import setup, find_packages
 
-from setuptools import find_packages, setup
-
-here = os.path.abspath(os.path.dirname(__file__))
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
-
-VERSION = "0.4.2"
-
-PACKAGE_NAME = "Evaluation machine translation"
-DESCRIPTION = ""
-LONG_DESCRIPTION = "common methods used to evaluate machine translation,"
-AUTHOR_NAME = "Alireza Parvaresh"
-AUTHOR_EMAIL = "parvvaresh@gmail.com"
-PROJECT_URL = "https://github.com/parvvaresh/Evaluation-of-machine-translation-by-NLP"
-REQUIRED_PACKAGES = []
-PROJECT_KEYWORDS = ["nlp", "machine translation" , "bleu", "nist", "glue" ,"chrf", "wer" , "ter" , "meteor"]
-
-CLASSIFIERS = [
-    "Development Status :: 1 - Planning",
-    "Intended Audience :: Developers",
-    "Programming Language :: Python :: 3",
-    "Operating System :: Unix",
-    "Operating System :: MacOS :: MacOS X",
-    "Operating System :: Microsoft :: Windows",
-]
 setup(
-    name=PACKAGE_NAME,
-    version=VERSION,
-    author=AUTHOR_NAME,
-    author_email=AUTHOR_EMAIL,
-    description=DESCRIPTION,
-    url=PROJECT_URL,
+    name="evaluation-machine-translation", 
+    version="0.1.0",
+    author="Alireza Parvaresh",
+    author_email="parvvaresh@gmail.com",
+    description="common methods used to evaluate machine translation,",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    url="https://github.com/ItrcAiLabs/eval_mt",
     packages=find_packages(),
-    install_requires=REQUIRED_PACKAGES,
-    keywords=PROJECT_KEYWORDS,
-    classifiers=CLASSIFIERS,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
